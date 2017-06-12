510473de6e3f336372a87332db956098f50d74bb
ander <ander@DESKTOP-BIF09P4>
2017-06-12 09:22:52 +0200

    haha

---
+++ SummerProject/ColissionHandler.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SummerProject
+{
+    class ColissionHandler
+    {
+        public void CheckColissions(List<Collidable> list)
+        {
+            foreach (Collidable c1 in list)
+            {
+                foreach (Collidable c2 in list)
+                {
+                    c1.BoundBox.
+                }
+            }
+        }
+    }
+}
