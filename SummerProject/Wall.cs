46a2104b64c0e9dce55ebe9c0b61093beed59951
Oskar <Oskar@OWPC>
2017-06-12 11:34:51 +0200

    small commit

---
+++ SummerProject/Wall.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SummerProject
+{
+    class Wall 
+    {
+        private Vector2 position;
+        private float angle = 0;
+        private Sprite sprite;
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            sprite.position = position;
+            sprite.rotation = angle;
+            sprite.Draw(spriteBatch);
+        }
+    }
+}
