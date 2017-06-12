04099f5b698b8995743f9329ab03d62f6a149db6
Oskar <Oskar@OWPC>
2017-06-12 14:16:12 +0200

    fixed enemy

--- SummerProject/Enemy.cs
+++ SummerProject/Enemy.cs
@@ -16,11 +16,11 @@ namespace SummerProject
         private Player player;
         private float speed = 0.5f;
         public Enemy(Vector2 position, Sprite sprite, Player player)
+            : base(sprite.spriteRect.Width, sprite.spriteRect.Height)
         {
             Position = position;
             this.player = player;
             this.sprite = sprite;
-            BoundBox = new Rectangle((int)Math.Round(Position.X), (int)Math.Round(Position.Y), sprite.spriteRect.Width, sprite.spriteRect.Height);
         }
 
         public void Update()
@@ -46,7 +46,6 @@ namespace SummerProject
         private void Move()
         {
             Position = new Vector2(Position.X + (float)Math.Cos(angle) * speed, Position.Y + (float)Math.Sin(angle) * speed);
-            BoundBox = new Rectangle((int)Math.Round(Position.X), (int)Math.Round(Position.Y), BoundBox.Width, BoundBox.Height);
         }
 
 
@@ -56,6 +55,6 @@ namespace SummerProject
             sprite.rotation = angle;
             sprite.Draw(spriteBatch);
         }
-
+ 
     }
 }
