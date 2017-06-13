2f88f80d887dcbaaf5ff3f0ac2b5748c5fe86460
ander <ander@DESKTOP-BIF09P4>
2017-06-13 15:13:44 +0200

    homing testbulllet kinda works

--- SummerProject/collidables/BulletTest.cs
+++ SummerProject/collidables/BulletTest.cs
@@ -8,16 +8,15 @@ using Microsoft.Xna.Framework.Graphics;
 
 namespace SummerProject
 {
-    class BulletTest : Projectile
+    class BulletTest : collidables.Projectile
     {
         private Enemy enemy;
         private bool lockedOn;
         private Rectangle oldBoundBox;
         private Rectangle bigBoundBox;
-        public BulletTest(Sprite sprite) : base(sprite)
+        public BulletTest(Sprite sprite) : base(Vector2.Zero, sprite)
         {
-            this.sprite = sprite;
-            Damage = 10; //!   
+            Damage = 0; //!   
             oldBoundBox = BoundBox;
             bigBoundBox = new Rectangle(oldBoundBox.X, oldBoundBox.Y, 200, 200);
             BoundBox = bigBoundBox;
@@ -31,13 +30,6 @@ namespace SummerProject
         }
 
 
-        protected override void Move()
-        {
-            if (lockedOn)
-                CalculateAngle();
-            Position = new Vector2(Position.X + (float)Math.Cos(angle), Position.Y + (float)Math.Sin(angle));
-        }
-
         public override void Collision(Collidable c2)
         {
             if (!lockedOn && c2 is Enemy)
@@ -45,13 +37,31 @@ namespace SummerProject
                 enemy = (Enemy)c2;
                 lockedOn = true;
                 BoundBox = oldBoundBox;
+                Damage = 10;
             }
-            else if (lockedOn && (c2 is Enemy || c2 is Wall))
+            else if ((lockedOn && c2 is Enemy) || c2 is Wall)
             {
                 Death();
                 lockedOn = false;
                 BoundBox = bigBoundBox;
             }
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            if (lockedOn)
+                CalculateAngle();
+            UpdateTimer(gameTime);
+            Move();
+        }
+
+        protected override void SpecificActivation(Vector2 source, Vector2 target)
+        {
+            float dX = source.X - target.X;
+            float dY = source.Y - target.Y;
+            base.CalculateAngle(dX, dY);
+            Damage = 0;
+            ResetSpawnTime();
         }
     }
 }
--- SummerProject/collidables/Enemy.cs
+++ SummerProject/collidables/Enemy.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using SummerProject.collidables;
 
 namespace SummerProject
 {
@@ -45,9 +46,9 @@ namespace SummerProject
        
         public override void Collision(Collidable c2)
         {
-            if (c2 is Bullet)
+            if (c2 is Projectile)
             {
-                Bullet b = c2 as Bullet;
+                Projectile b = c2 as Projectile;
                 Health -= b.Damage;
             }
             if (c2 is Player)
