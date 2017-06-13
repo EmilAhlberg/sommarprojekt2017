6331b20d2fa517fbca76020d7ef6eedaee565f1e
Jorav <johan@ravnborg.se>
2017-06-13 10:36:57 +0200

    Refactored speed property

--- SummerProject/Movable.cs
+++ SummerProject/Movable.cs
@@ -9,7 +9,7 @@ namespace SummerProject
 {
     public abstract class Movable : Drawable
     {
-        protected float speed { set; get; } = 1f;
+        protected float Speed { set; get; } = 1f;
 
         public Movable(Vector2 position, Sprite sprite) : base(position, sprite) { }
 
@@ -27,7 +27,7 @@ namespace SummerProject
 
         protected Vector2 Move(int TODO)
         {
-            return new Vector2(Position.X + (float)Math.Cos(angle) * speed, Position.Y + (float)Math.Sin(angle) * speed); //TODO
+            return new Vector2(Position.X + (float)Math.Cos(angle) * Speed, Position.Y + (float)Math.Sin(angle) * Speed); //TODO
         }
 
         protected abstract void Move();
