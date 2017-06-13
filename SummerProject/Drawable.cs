22952d0d3ed06ff279b2807944e49f1f4827d9fc
Jorav <johan@ravnborg.se>
2017-06-13 10:00:43 +0200

    Moved CalculateAngle to correct class

--- SummerProject/Drawable.cs
+++ SummerProject/Drawable.cs
@@ -20,17 +20,6 @@ namespace SummerProject
             sprite.origin = new Vector2(sprite.spriteRect.Width / 2, sprite.spriteRect.Height / 2);
         }
 
-        protected void CalculateAngle(float dX, float dY)
-        {
-            if (dX != 0)
-            {
-                angle = (float)Math.Atan(dY / dX);
-            }
-            if (dX > 0)
-                angle += (float)Math.PI;
-
-            angle = angle % (2 * (float)Math.PI);
-        }
         public void Draw(SpriteBatch spriteBatch)
         {
             sprite.position = Position;
