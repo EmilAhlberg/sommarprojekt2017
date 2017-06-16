ec39c6bb94a62d12e250aef384a2f7ed91db13ba
Jorav <johan@ravnborg.se>
2017-06-16 09:18:30 +0200

    Collidable refactor, fixed bug in RotRectangle

--- SummerProject/RotRectangle.cs
+++ SummerProject/RotRectangle.cs
@@ -15,7 +15,31 @@ namespace SummerProject
         private Vector2 UR { set; get; }
         public int Width { set; get; }
         public int Height { set; get; }
-        //public Point Location { set; get; }
+        public Vector2 origin { get; }
+        private float angle { set; get; } = 0;
+        public float Angle
+        {
+            set
+            {
+                Rotate(value-angle);
+            }
+            get { return angle;}
+        }
+        public Vector2 Location
+        {
+            set
+            {
+                Vector2 DRUL = Vector2.Divide(Vector2.Subtract(DR, UL), 2);
+                Vector2 URDL = Vector2.Divide(Vector2.Subtract(UR, DL), 2);
+                UL = Vector2.Subtract(value, DRUL);
+                DR = Vector2.Add(value, DRUL);
+                DL = Vector2.Subtract(value, URDL);
+                UR = Vector2.Add(value, URDL);
+
+            }
+            get { return Vector2.Add(UL, Vector2.Divide(Vector2.Subtract(DR, UL), 2)); }
+        }
+
 
         public RotRectangle(Rectangle rect, float angleRad)
         {
@@ -23,27 +47,26 @@ namespace SummerProject
             this.DL = new Vector2(rect.Left, rect.Bottom);
             this.DR = new Vector2(rect.Right, rect.Bottom);
             this.UR = new Vector2(rect.Right, rect.Top);
-            //Location = rect.Location;
             Width = rect.Width;
             Height = rect.Height;
             Rotate(angleRad);
+            origin = Vector2.Divide(Vector2.Subtract(DR, UL), 2);
         }
 
         public void Rotate(float angleRad)
         {
+            angle = (angle + angleRad) % (float)(2 * Math.PI);
+            Vector2 loc = new Vector2(Location.X, Location.Y);
             Matrix rotation = Matrix.CreateRotationZ(angleRad);
-            Vector2 pos = new Vector2((DR.X + UL.X)/2, (DR.Y + UL.Y)/2);
-            UL = Vector2.Add(pos, Vector2.Transform(UL - pos, rotation));
-            DL = Vector2.Add(pos, Vector2.Transform(DL-pos, rotation));
-            DR = Vector2.Add(pos, Vector2.Transform(DR - pos, rotation));
-            UR = Vector2.Add(pos, Vector2.Transform(UR - pos, rotation));
+            UL = Vector2.Add(loc, Vector2.Transform(UL - loc, rotation));
+            DL = Vector2.Add(loc, Vector2.Transform(DL - loc, rotation));
+            DR = Vector2.Add(loc, Vector2.Transform(DR - loc, rotation));
+            UR = Vector2.Add(loc, Vector2.Transform(UR - loc, rotation));
         }
 
         public bool Intersects(RotRectangle r)
         {
-            //Console.WriteLine("Coord1 : " + UL + ", " + DL + ", " + DR + ", " + UR + " Coord2 : " + r.UL + ", " + r.DL + ", " + r.DR + ", " + r.UR);
             Vector2[] axes = GenerateAxes(r);
-            //Console.WriteLine("Axes : " + axes[0] + ", " + axes[1] + ", " + axes[2] + ", " + axes[3]);
             float[] scalarA = new float[4];
             float[] scalarB = new float[4];
             foreach (Vector2 axis in axes)
@@ -56,23 +79,13 @@ namespace SummerProject
                 scalarB[1] = Vector2.Dot(axis, Vector2.Multiply(axis, Vector2.Dot(r.DL, axis) / axis.LengthSquared()));
                 scalarB[2] = Vector2.Dot(axis, Vector2.Multiply(axis, Vector2.Dot(r.DR, axis) / axis.LengthSquared()));
                 scalarB[3] = Vector2.Dot(axis, Vector2.Multiply(axis, Vector2.Dot(r.UR, axis) / axis.LengthSquared()));
-                //Console.WriteLine("A0 : " + scalarA[0]);
-                //Console.WriteLine("A1 : " + scalarA[1]);
-                //Console.WriteLine("A2 : " + scalarA[2]);
-                //Console.WriteLine("A3 : " + scalarA[3]);
-                //Console.WriteLine("B0 : " + scalarB[0]);
-                //Console.WriteLine("B1 : " + scalarB[1]);
-                //Console.WriteLine("B2 : " + scalarB[2]);
-                //Console.WriteLine("B3 : " + scalarB[3]);
-                //Console.WriteLine("A: min - " + scalarA.Min() + " max - " + scalarA.Max() + " B: min - " + scalarB.Min() + " max - " + scalarB.Max());
-               // if (!(scalarB.Min() <= scalarA.Max() && scalarB.Max() >= scalarA.Min()))
                if(scalarB.Max() < scalarA.Min() || scalarA.Max() < scalarB.Min() )
                     return false;
             }
             return true;
         }
 
-        public Vector2[] GenerateAxes(RotRectangle r)
+        private Vector2[] GenerateAxes(RotRectangle r)
         {
             Vector2[] axes = new Vector2[4];
             axes[0] = new Vector2(UR.X - UL.X, UR.Y - UL.Y);
@@ -83,4 +96,3 @@ namespace SummerProject
         }
     }
 }
-//Vector2 v = Vector2.Divide(Vector2.Multiply(axis, Vector2.Dot(UL, axis)), axis.LengthSquared());
