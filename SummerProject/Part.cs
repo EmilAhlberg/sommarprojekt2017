9aa4dd054faa13878bf0ba0308c232b6953ff91c
Jorav <johan@ravnborg.se>
2017-06-19 15:50:25 +0200

    Part-skeleton added

---
+++ SummerProject/Part.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+
+namespace SummerProject
+{
+    public abstract class Part : Collidable
+    {
+        protected abstract CompositePart Hull {set; get; }
+
+        public Part(Vector2 position, ISprite sprite, CompositePart Hull) : base(position, sprite)
+        {
+            this.Hull = Hull;
+        }
+
+    }
+}
