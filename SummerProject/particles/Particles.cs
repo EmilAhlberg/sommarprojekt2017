9d04397bf3af304baaf4426c36e005b8ae99f562
Oskar <Oskar@OWPC>
2017-06-19 12:03:47 +0200

    refactored particles

--- SummerProject/particles/Particles.cs
+++ SummerProject/particles/Particles.cs
@@ -8,6 +8,7 @@ namespace SummerProject
     public static class Particles
     {
         static List<Particle> particles;
+        static Random rand;
         static List<Sprite> spriteList;
         private const int maxParticles = 100;
 
@@ -16,6 +17,7 @@ namespace SummerProject
             particles = new List<Particle>();
             spriteList = new List<Sprite>();
             spriteList.Add(new Sprite());
+            rand = new Random();
         }
 
         public static void AddSprite(Sprite s)
@@ -46,7 +48,6 @@ namespace SummerProject
             Color color = Color.White;
             float scale = 1;
             float ttl = 1;
-            Random rand = new Random();
 
             switch (ID)
             {
@@ -61,19 +62,10 @@ namespace SummerProject
                 #region Enemy Explosion
                 case 2:
                     {
-                        initialForce = 30*new Vector2((float)Math.Cos(angle), (float)Math.Sin(angle));
+                        initialForce = 30 * new Vector2((float)Math.Cos(angle), (float)Math.Sin(angle));
                         particles.Add(new Particle(new Sprite(spriteList[1]), position, initialForce, angle, angularVelocity, color, scale, ttl, 2));
                         particles.Add(new Particle(new Sprite(spriteList[2]), position, -initialForce, angle, angularVelocity, color, scale, ttl, 2));
-                        for (int i = 0; i < 10; i++)
-                        {
-                            initialForce = new Vector2(2 * (float)rand.NextDouble() - 1, 2 * (float)rand.NextDouble() - 1);
-                            initialForce.Normalize();
-                            initialForce *= 20 * (float)rand.NextDouble() + 60;
-                            color = Color.MonoGameOrange;
-                            angularVelocity = 0.2f;
-                            scale = (float)rand.NextDouble() + 1;
-                            particles.Add(new Particle(new Sprite(spriteList[0]), position, initialForce, (float)Math.Atan2(initialForce.Y, initialForce.X), angularVelocity, color, scale, ttl, 1));
-                        }
+                        CreateExplosion(10, position, 10, 40, 0.2f, Color.MonoGameOrange, 1, 1, ttl);
                         break;
                     }
                 #endregion
@@ -81,19 +73,10 @@ namespace SummerProject
                 #region Player Explosion
                 case 3:
                     {
-                        initialForce = 30*new Vector2((float)Math.Cos(angle), (float)Math.Sin(angle));
+                        initialForce = 30 * new Vector2((float)Math.Cos(angle), (float)Math.Sin(angle));
                         particles.Add(new Particle(new Sprite(spriteList[3]), position, initialForce, angle, angularVelocity, color, scale, ttl, 2));
                         particles.Add(new Particle(new Sprite(spriteList[4]), position, -initialForce, angle, angularVelocity, color, scale, ttl, 2));
-                        for (int i = 0; i < 100; i++)
-                        {
-                            initialForce = new Vector2(2 * (float)rand.NextDouble() - 1, 2 * (float)rand.NextDouble() - 1);
-                            initialForce.Normalize();
-                            initialForce *= 30 * (float)rand.NextDouble() + 100;
-                            color = Color.CornflowerBlue;
-                            angularVelocity = 0.1f;
-                            scale = (float)rand.NextDouble() + 1;
-                            particles.Add(new Particle(new Sprite(spriteList[0]), position, initialForce, (float)Math.Atan2(initialForce.Y, initialForce.X), angularVelocity, color, scale, ttl, 1));
-                        }
+                        CreateExplosion(100, position, 20, 80, 0.2f, Color.CornflowerBlue, 1, 1, ttl);
                         break;
                     }
                 #endregion
@@ -101,15 +84,7 @@ namespace SummerProject
                 #region Thruster Trail
                 case 4:
                     {
-                        initialForce = -new Vector2((float)Math.Cos(angle), (float)Math.Sin(angle));
-                        {
-                            initialForce.Normalize();
-                            ttl = 0.3f;
-                            initialForce *= 5*(float)rand.NextDouble() + 40;
-                            color = Color.MonoGameOrange;
-                            scale = 2;
-                            particles.Add(new Particle(new Sprite(spriteList[0]), position, initialForce, (float)Math.Atan2(initialForce.Y, initialForce.X), angularVelocity, color, scale, ttl, 1));
-                        }
+                        CreateTrail(angle, position, 5, 40, 0, Color.MonoGameOrange, 0, 2, 0.3f);
                         break;
                     }
                 #endregion
@@ -117,16 +92,7 @@ namespace SummerProject
                 #region Bullet Explosion
                 case 5:
                     {
-                        for (int i = 0; i < 10; i++)
-                        {
-                            initialForce = new Vector2(2 * (float)rand.NextDouble() - 1, 2 * (float)rand.NextDouble() - 1);
-                            initialForce.Normalize();
-                            initialForce *= 10 * (float)rand.NextDouble() + 40;
-                            color = Color.CornflowerBlue;
-                            angularVelocity = 0.5f;
-                            scale = 1 * (float)rand.NextDouble() + 0.5f;
-                            particles.Add(new Particle(new Sprite(spriteList[0]), position, initialForce, (float)Math.Atan2(initialForce.Y, initialForce.X), angularVelocity, color, scale, ttl, 1));
-                        }
+                        CreateExplosion(10, position, 10, 20, 0.5f, Color.CornflowerBlue, 1, 0.5f, ttl);
                         break;
                     }
                 #endregion
@@ -134,15 +100,7 @@ namespace SummerProject
                 #region Bullet Trail
                 case 6:
                     {
-                        initialForce = -new Vector2((float)Math.Cos(angle), (float)Math.Sin(angle));
-                        {
-                            initialForce.Normalize();
-                            ttl = 0.3f;
-                            initialForce *= (float)rand.NextDouble() + 20;
-                            color = Color.CornflowerBlue;
-                            scale = 1;
-                            particles.Add(new Particle(new Sprite(spriteList[0]), position, initialForce, (float)Math.Atan2(initialForce.Y, initialForce.X), angularVelocity, color, scale, ttl, 1));
-                        }
+                        CreateTrail(angle, position, 1, 20, 0, Color.CornflowerBlue, 0, 1, 0.3f);
                         break;
                     }
                     #endregion
@@ -156,6 +114,37 @@ namespace SummerProject
                 if (p.IsActive)
                     p.Draw(sb, gameTime);
             }
+        }
+
+        private static void CreateExplosion(int nbrOfParticles, Vector2 position, float spread, float baseValue, float angularVelocity, Color color, float scaleSpread, float baseScale, float ttl)
+        {
+            for (int i = 0; i < nbrOfParticles; i++)
+            {
+                Vector2 initialForce = RandomVector2(spread, baseValue);
+                float scale = RandomFloat(scaleSpread, baseScale);
+                particles.Add(new Particle(new Sprite(spriteList[0]), position, initialForce, (float)Math.Atan2(initialForce.Y, initialForce.X), angularVelocity, color, scale, ttl, 1));
+            }
+        }
+
+        private static void CreateTrail(float angle, Vector2 position, float spread, float baseValue, float angularVelocity, Color color, float scaleSpread, float baseScale, float ttl)
+        {
+            Vector2 initialForce = -new Vector2((float)Math.Cos(angle), (float)Math.Sin(angle));
+            initialForce *= RandomFloat(spread, baseValue);
+            float scale = RandomFloat(scaleSpread, baseScale);
+            particles.Add(new Particle(new Sprite(spriteList[0]), position, initialForce, (float)Math.Atan2(initialForce.Y, initialForce.X), angularVelocity, color, scale, ttl, 1));
+        }
+
+        private static Vector2 RandomVector2(float spread, float baseValue)
+        {
+            Vector2 v = new Vector2(2 * (float)rand.NextDouble() - 1, 2 * (float)rand.NextDouble() - 1);
+            v.Normalize();
+            v *= RandomFloat(spread, baseValue);
+            return v;
+        }
+
+        private static float RandomFloat(float spread, float baseValue)
+        {
+            return (float)rand.NextDouble() * spread + baseValue;
         }
     }
 }
