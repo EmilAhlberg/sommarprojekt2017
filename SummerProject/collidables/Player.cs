b8aa95223cc10546e5a528b84a66b076a1e7b8d2
ander <ander@DESKTOP-BIF09P4>
2017-06-20 14:41:40 +0200

    Hoaming bullet working

--- SummerProject/collidables/Player.cs
+++ SummerProject/collidables/Player.cs
@@ -46,7 +46,7 @@ namespace SummerProject.collidables
                     CalculateAngle();
                 Particles.GenerateParticles(Position, 4, angle);
                 Move();
-                //HandleBulletType();
+                HandleBulletType();
                 Fire();
                 if (Health <= 0 && !IsDead)
                     Death();
@@ -54,23 +54,21 @@ namespace SummerProject.collidables
                 {
                     Energy -= shieldDischargeRate;
                     shieldOn = true;
-                    ActiveBoundBoxIndex = 1;
                 }
                 else if(maxEnergy > Energy)
                 {
                     Energy += shieldRechargeRate;
                     shieldOn = false;
-                    ActiveBoundBoxIndex = 0;
                 }
             }
         }
 
         private void HandleBulletType()
         {
-            //if (Keyboard.GetState().IsKeyDown(Keys.D1))
-            //     projectiles.switchBullets(EntityTypes.BULLET);
-            //  if (Keyboard.GetState().IsKeyDown(Keys.D2))
-            //       projectiles.switchBullets(EntityTypes.HOMINGBULLET);
+            if (Keyboard.GetState().IsKeyDown(Keys.D1))
+                projectiles.SwitchBullets(EntityTypes.BULLET);
+            if (Keyboard.GetState().IsKeyDown(Keys.D2))
+                projectiles.SwitchBullets(EntityTypes.HOMINGBULLET);
         }
 
         private void Fire()
