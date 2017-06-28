470e52b1659cfe2a0555586138e6791b5fc0a471
Oskar <Oskar@OWPC>
2017-06-28 09:43:58 +0200

    fxxxxxxxxxxx

---
+++ SummerProject/util/DrawHelper.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SummerProject.util
+{
+    static class DrawHelper
+    {
+        public static void DrawOutlinedString(SpriteBatch sb, float outlineSize, Color outlineColor, SpriteFont font, string text, Vector2 position, Color color, float rotation, Vector2 origin, float scale)
+        {
+            sb.DrawString(font, text, position + new Vector2(outlineSize, outlineSize), outlineColor, rotation, origin, scale, SpriteEffects.None, 1);
+            sb.DrawString(font, text, position + new Vector2(-outlineSize, -outlineSize), outlineColor, rotation, origin, scale, SpriteEffects.None, 1);
+            sb.DrawString(font, text, position + new Vector2(-outlineSize, outlineSize), outlineColor, rotation, origin, scale, SpriteEffects.None, 1);
+            sb.DrawString(font, text, position + new Vector2(outlineSize, -outlineSize), outlineColor, rotation, origin, scale, SpriteEffects.None, 1);
+            sb.DrawString(font, text, position + new Vector2(outlineSize, 0), outlineColor, rotation, origin, scale, SpriteEffects.None, 1);
+            sb.DrawString(font, text, position + new Vector2(-outlineSize, -0), outlineColor, rotation, origin, scale, SpriteEffects.None, 1);
+            sb.DrawString(font, text, position + new Vector2(-0, outlineSize), outlineColor, rotation, origin, scale, SpriteEffects.None, 1);
+            sb.DrawString(font, text, position + new Vector2(0, -outlineSize), outlineColor, rotation, origin, scale, SpriteEffects.None, 1);
+            sb.DrawString(font, text, position, color, rotation, origin, scale, SpriteEffects.None, 1);
+        }
+        public static void DrawExtrudedString(SpriteBatch sb, float outlineSize, Color outlineColor, SpriteFont font, string text, Vector2 position, Color color, float rotation, Vector2 origin, float scale)
+        {
+            sb.DrawString(font, text, position + new Vector2(outlineSize, -outlineSize), Color.Black, rotation, origin, scale, SpriteEffects.None, 1);
+            sb.DrawString(font, text, position + new Vector2(-outlineSize, outlineSize), Color.White, rotation, origin, scale, SpriteEffects.None, 1);
+            sb.DrawString(font, text, position + new Vector2(outlineSize, outlineSize), Color.Black, rotation, origin, scale, SpriteEffects.None, 1);
+            sb.DrawString(font, text, position + new Vector2(outlineSize, 0), Color.Black, rotation, origin, scale, SpriteEffects.None, 1);
+            sb.DrawString(font, text, position + new Vector2(-0, outlineSize), Color.Black, rotation, origin, scale, SpriteEffects.None, 1);
+            sb.DrawString(font, text, position + new Vector2(0, -outlineSize), Color.White, rotation, origin, scale, SpriteEffects.None, 1);
+            sb.DrawString(font, text, position + new Vector2(-outlineSize, -0), Color.White, rotation, origin, scale, SpriteEffects.None, 1);
+            sb.DrawString(font, text, position + new Vector2(-outlineSize, -outlineSize), Color.White, rotation, origin, scale, SpriteEffects.None, 1);
+            sb.DrawString(font, text, position, color, rotation, origin, scale, SpriteEffects.None, 1);
+        }
+    }
+}
