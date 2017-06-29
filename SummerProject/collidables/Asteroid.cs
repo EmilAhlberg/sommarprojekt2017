e95c2d3a7c4bdafdf2a4af575124e21f11623719
ander <ander@DESKTOP-BIF09P4>
2017-06-29 11:03:53 +0200

    asteroid class

---
+++ SummerProject/collidables/Asteroid.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+
+namespace SummerProject.collidables
+{
+    class Asteroid : Enemy
+    {
+        public Asteroid(Vector2 position, ISprite sprite, Player player) : base(position, sprite, player)
+        {
+            WorthScore = 0;
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            Move();
+        }
+
+        protected override void SpecificActivation(Vector2 source, Vector2 target)
+        {
+            Health = EntityConstants.HEALTH[EntityConstants.ENEMY] * 2;
+            Thrust = EntityConstants.THRUST[EntityConstants.ENEMY];
+            float dX = Position.X - target.X;
+            float dY = Position.Y - target.Y;
+            base.CalculateAngle(dX, dY);
+        }
+    }
+}
