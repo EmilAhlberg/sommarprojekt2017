e0da290fd581f0458c896ca1f262b07d80c8603f
ander <ander@DESKTOP-BIF09P4>
2017-07-12 15:11:37 +0200

    Gun names

--- SummerProject/collidables/EntityConstants.cs
+++ SummerProject/collidables/EntityConstants.cs
@@ -73,7 +73,7 @@ namespace SummerProject.collidables
             return dictionary;
         }
 
-        public static float GetStatsFromID(Dictionary<int, float> dic, IDs id)
+        public static T GetStatsFromID<T>(Dictionary<int, T> dic, IDs id)
         {
             while (!dic.Keys.Contains((int)id))
             {
@@ -170,6 +170,19 @@ namespace SummerProject.collidables
                 {(int)IDs.RECTHULLPART, 8000},
                 {(int)IDs.ENGINEPART, 14000},
                 {(int)IDs.DEFAULT_PART, 15000}
+        };
+
+        public static readonly Dictionary<int, string> NAME =
+        new Dictionary<int, string>
+        {
+                {(int)IDs.GUNPART, "Sniping Gun"},
+                {(int)IDs.EMPTYPART, "Sell Parts"},
+                {(int)IDs.SPRAYGUNPART, "Spray Gun"},
+                {(int)IDs.MINEGUNPART, "Mine Deployer"},
+                {(int)IDs.CHARGINGGUNPART, "Fusion Gun"},
+                {(int)IDs.RECTHULLPART, "Hull"},
+                {(int)IDs.ENGINEPART, "Engine"},
+                {(int)IDs.DEFAULT_PART, "NO_NAME"}
         };
     }
 }
